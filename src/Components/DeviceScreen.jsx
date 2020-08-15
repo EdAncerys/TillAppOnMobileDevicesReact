9f7ec9cda9ececfa0ebd91ae93a3af7d84a01516
Ed <lookatemail@gmail.com>
2020-08-15 13:00:47 +0100

    Add project color palet

--- src/Components/DeviceScreen.jsx
+++ src/Components/DeviceScreen.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import colors from '../config/colors';
 
 export default function DeviceScreen({ height = '30vh', width = '20vw' }) {
   return (
     <div
       style={{
         display: 'grid',
-        alignContent: 'center',
         justifyContent: 'center',
-        backgroundColor: '#ccc',
+        backgroundColor: colors.light,
+        border: `5px solid ${colors.black}`,
         height: height,
         width: width,
         borderRadius: 20,
