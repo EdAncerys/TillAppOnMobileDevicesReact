e750440ad6a2ee78ac09c45d84236bdbe591fb89
Ed <lookatemail@gmail.com>
2020-08-15 15:29:53 +0100

    Add onMouseEnter/Leave event for button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import colors from '../config/colors';
 
 export default function Button({ text = 'Button', width = '100%', onClick }) {
+  const [hover, setHover] = useState(false);
   return (
     <div
       style={{
         display: 'grid',
-        backgroundColor: colors.primary,
+        backgroundColor: hover ? colors.secondary : colors.primary,
         borderRadius: 25,
         justifyContent: 'center',
         alignItems: 'center',
         width: width,
+        cursor: hover ? 'pointer' : '',
       }}
       onClick={onClick}
+      onMouseEnter={() => setHover(!hover)}
+      onMouseLeave={() => setHover(!hover)}
     >
       <p style={styles.text}>{text}</p>
     </div>
