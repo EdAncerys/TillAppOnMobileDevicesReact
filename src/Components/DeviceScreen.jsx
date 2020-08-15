060f915a66a65fc24bd79d89da9cec6181be98b9
Ed <lookatemail@gmail.com>
2020-08-15 13:07:37 +0100

    Refactor DeviceScreen component

--- src/Components/DeviceScreen.jsx
+++ src/Components/DeviceScreen.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import colors from '../config/colors';
 
-export default function DeviceScreen({ height = '30vh', width = '20vw' }) {
+export default function DeviceScreen({
+  children,
+  height = '30vh',
+  width = '20vw',
+}) {
   return (
     <div
       style={{
@@ -9,12 +13,13 @@ export default function DeviceScreen({ height = '30vh', width = '20vw' }) {
         justifyContent: 'center',
         backgroundColor: colors.light,
         border: `5px solid ${colors.black}`,
+        borderBottom: `35px solid ${colors.black}`,
         height: height,
         width: width,
         borderRadius: 20,
       }}
     >
-      <p>Im a device screen</p>
+      <>{children}</>
     </div>
   );
 }
--- src/Pages/FrontPage.jsx
+++ src/Pages/FrontPage.jsx
@@ -5,7 +5,9 @@ export default function FrontPage() {
   return (
     <div>
       <h1>This is Front Page for CSS Grid</h1>
-      <DeviceScreen />
+      <DeviceScreen>
+        <p>This is a device</p>
+      </DeviceScreen>
     </div>
   );
 }
